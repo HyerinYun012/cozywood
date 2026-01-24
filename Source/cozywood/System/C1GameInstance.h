77b768e5bbf835e6d9f56b124746a175d70e0f92
HyerinYun012 <hrin3088@tukorea.ac.kr>
2026-01-24 20:56:40 +0900

    초기 설정2

---
+++ Source/cozywood/System/C1GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "C1GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COZYWOOD_API UC1GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
